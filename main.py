b485667fa6b14ece99435b3bc5476c4d497e1437
Gaz <karimovgaz72@gmail.com>
2025-03-23 20:25:12 +0500

    Добавил обработчик ошибок ввода не числа, дописал блок выводящий сообщение об ошибке при вводе не целого и не положительного числа.

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-number = int(input("Введите целое положительное число - "))
+try:
+    number = int(input("Введите целое положительное число - "))
 
-while number >= 0:
-    print(number)
-    number -= 1
+    while number >= 0:
+        print(number)
+        number -= 1
+    else:
+        print("Введенно не целое положительное число. ")
+except ValueError:
+    print("Ошибка: введено не число!")
