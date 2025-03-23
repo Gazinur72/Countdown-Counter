9a4f1f5439e7931b07240a251a79c3953bff9c2c
Gaz <karimovgaz72@gmail.com>
2025-03-23 21:53:24 +0500

    Написал проверку на ввод отрицательного числа через If else

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 try:
     number = int(input("Введите целое положительное число - "))
-
-    while number >= 0:
-        print(number)
-        number -= 1
+    if  number >= 0:
+        while number >= 0:
+            print(number)
+            number -= 1
     else:
-        print("Введенно не целое положительное число. ")
+        print("Вы ввели отрицательно число.")
 except ValueError:
     print("Ошибка: введено число не соответствующее параметрам ввода!!!")
